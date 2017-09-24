76240e1e86d6c118daeffccbcb14195f97f48aa6
modlinski <modlinski@modlinski-GL72-6QC>
2017-09-24 21:36:42 +0200

    Add timeout.

--- solutions/unittest_part/test_my_patients_selenium.py
+++ solutions/unittest_part/test_my_patients_selenium.py
@@ -1,6 +1,9 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class MyPatientsTestCase(unittest.TestCase):
@@ -30,7 +33,8 @@ class MyPatientsTestCase(unittest.TestCase):
         self._login('doctor_a@example.com', 'admin123')
 
         # Narrowing web element, to be sure that link comes from nav
-        nav = self.browser.find_element_by_css_selector('.nav-sidebar')
+        wait = WebDriverWait(browser, 10)
+        nav = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.nav-sidebar')))
         nav.find_element_by_link_text("My patients").click()
 
         expected_headers = ['Email', 'First Name', 'Last Name']
